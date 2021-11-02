52d963156a9574f91d0863ac2e04f723fd9c0005
MitchWijt <mitchel@wijt.net>
2021-11-02 08:49:00 -0400

    feat: add basic lighting to the cube

--- src/Shaders/Object/VertexShader.glsl
+++ src/Shaders/Object/VertexShader.glsl
@@ -2,8 +2,11 @@
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoord;
+layout (location = 2) in vec3 aNormal;
 
 out vec2 texCoord;
+out vec3 normal;
+out vec3 fragPos;
 
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
@@ -12,5 +15,7 @@ uniform mat4 projectionMatrix;
 void main()
 {
     texCoord = aTexCoord;
+    normal = aNormal;
     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(aPos, 1.0);
+    fragPos = vec3(modelMatrix * vec4(aPos, 1.0));
 }
